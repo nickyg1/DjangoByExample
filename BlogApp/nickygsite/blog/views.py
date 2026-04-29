38fdb7c73ea5eb2c041b488ae1565d42e4d783e5
Nicolas Gallo <nicolaslucasgallo@gmail.com>
2026-04-29 15:57:35 -0500

    pg 55-72
    +canonical urls
    +SEO friendly urls
    +Added pagination
    +Handled pagination erros

--- BlogApp/nickygsite/blog/views.py
+++ BlogApp/nickygsite/blog/views.py
@@ -1,8 +1,19 @@
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.views.generic import ListView
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(
         request,
         'blog/post/list.html',
@@ -10,17 +21,21 @@ def post_list(request):
     )
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
-    )
-
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
     return render(
         request,
         'blog/post/detail.html',
-        {
-            'post': post,
-        }
+        {'post':post}
     )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
