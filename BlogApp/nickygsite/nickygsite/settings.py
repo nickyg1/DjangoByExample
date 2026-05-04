12e5c542c8396a3abcbccd3de6ea364d1de0ca77
Nicolas Gallo <nicolaslucasgallo@gmail.com>
2026-05-04 13:07:24 -0500

    Finished chapter 2
    There is a bug with the comment form. Need to fix it before moving on

--- BlogApp/nickygsite/nickygsite/settings.py
+++ BlogApp/nickygsite/nickygsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,8 +28,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#Email server configuration
+EMAIL_HOST = 'stmp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
-# Application definition
+                                                                                                                     # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
