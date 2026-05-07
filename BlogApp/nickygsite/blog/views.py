56ed364880b8ebb4ac4562c58499e2b651d842c0
Nicolas Gallo <nicolaslucasgallo@gmail.com>
2026-05-07 12:31:35 -0500

    108-118
    Added tags to blog which can be searched

--- BlogApp/nickygsite/blog/views.py
+++ BlogApp/nickygsite/blog/views.py
@@ -6,10 +6,15 @@ from django.views.generic import ListView
 
 from .forms import CommentForm, EmailPostForm
 from .models import Post
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -24,7 +29,10 @@ def post_list(request):
     return render(
         request,
         'blog/post/list.html',
-        {'posts': posts}
+        {
+            'posts': posts,
+            'tag':tag
+         }
     )
 
 
